fc440582316480b04afc8408d0ed7f19e9761b77
Luckyboy1024 <2819549538@qq.com>
2020-03-29 10:44:48 +0800

    mv

--- test-20-03/test-20-03-15/test-20-03-15/test.c
+++
@@ -1,8 +0,0 @@
-#define _CRT_SECURE_NO_WARNINGS 1
-
-#define MAX 1000
-#define reg register
-#define do_forever for(;;)
-#define CASE break;case
-
-#define DEBUG_PRINT printf("file:%s\tline:%d\tdate:%s\ttime:%s\n", __FILE__, __LINE__, __DATE__, __TIME__)
